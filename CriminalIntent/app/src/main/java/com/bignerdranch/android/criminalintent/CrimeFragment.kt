471d1054b322680b254992a02679e73613e358f9
Kelian9995 <nsokovnin@gmail.com>
2022-10-16 16:33:34 +0300

    part 16.CameraIntent
    working with the file storage, choosing the location of the photo, using the camera intent, sending the intent, scaling

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -8,6 +8,7 @@ import android.content.pm.ResolveInfo
 import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
 import android.text.format.DateFormat
@@ -16,10 +17,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.fragment_crime.*
+import java.io.File
 import java.util.*
 
 private const val TAG = "CrimeFragment"
@@ -27,11 +30,14 @@ private const val ARG_CRIME_ID = "crime_id"
 private const val DIALOG_DATE = "DialogDate"
 private const val REQUEST_DATE = 0
 private const val REQUEST_CONTACT = 1
+private const val REQUEST_PHOTO = 2
 private const val DATE_FORMAT = "EEE, MMM, dd"
 
 class CrimeFragment : Fragment(), DatePickerFragment.Callbacks {
 
     private lateinit var crime: Crime
+    private lateinit var photoFile: File
+    private lateinit var photoUri: Uri
     private lateinit var titleField: EditText
     private lateinit var dateButton: Button
     private lateinit var solvedCheckBox: CheckBox
@@ -76,6 +82,9 @@ reportButton= view.findViewById(R.id.crime_report) as Button
             Observer { crime ->
                 crime?.let {
                     this.crime = crime
+                    photoFile = crimeDetailViewModel.getPhotoFile(crime)
+                    photoUri = FileProvider.getUriForFile(requireActivity(),
+                    "com.bignerdranch.android.criminalintent.fileprovider",photoFile )
                     updateUI()
                 }
             })
@@ -92,7 +101,8 @@ reportButton= view.findViewById(R.id.crime_report) as Button
                 count: Int,
                 after: Int
             ) {
-                // This space intentionally left blank
+
+
             }
 
             override fun onTextChanged(
@@ -146,11 +156,38 @@ reportButton= view.findViewById(R.id.crime_report) as Button
                 isEnabled = false
             }
         }
+        photoButton.apply {
+            val packageManager:PackageManager = requireActivity().packageManager
+            val captureImage = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val resolvedActivity: ResolveInfo?=
+                packageManager.resolveActivity(captureImage,PackageManager.MATCH_DEFAULT_ONLY)
+            if (resolvedActivity == null) {
+                isEnabled = false
+            }
+            setOnClickListener {
+                captureImage.putExtra(MediaStore.EXTRA_OUTPUT,photoUri)
+                val cameraActivities:List<ResolveInfo> =
+                    packageManager.queryIntentActivities(captureImage,PackageManager.MATCH_DEFAULT_ONLY)
+                for (cameraActivity in cameraActivities) {
+                    requireActivity().grantUriPermission(
+                        cameraActivity.activityInfo.packageName,photoUri,Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+
+                }
+                startActivityForResult(captureImage, REQUEST_PHOTO)
+            }
+        }
+        //return view
+
     }
 
     override fun onStop() {
         super.onStop()
         crimeDetailViewModel.saveCrime(crime)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        requireActivity().revokeUriPermission(photoUri,Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
     }
 
     override fun onDateSelected(date: Date) {
@@ -167,6 +204,15 @@ reportButton= view.findViewById(R.id.crime_report) as Button
         }
         if (crime.suspect.isNotEmpty()) {
             suspectButton.text = crime.suspect
+        }
+        updatePhotoView()
+    }
+    private fun updatePhotoView() {
+        if (photoFile.exists()){
+            val bitmap = getScaledBitmap(photoFile.path,requireActivity())
+            photoView.setImageBitmap(bitmap)
+        } else {
+            photoView.setImageDrawable(null)
         }
     }
 
@@ -196,6 +242,10 @@ reportButton= view.findViewById(R.id.crime_report) as Button
 
 
 
+            }
+            requestCode == REQUEST_PHOTO -> {
+                requireActivity().revokeUriPermission(photoUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+                updatePhotoView()
             }
         }
     }
